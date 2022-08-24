9947a7a04022a317ea61042ddd96f4aaa37c7144
galluccinahuel <galluccinahuel@gmail.com>
2022-08-24 00:30:28 -0300

    commit

---
+++ src/components/UI/App.jsx
@@ -0,0 +1,16 @@
+
+import { Navbar } from "./Navbar/Navbar";
+
+export function App(props){
+
+    const {parrafo} = props; 
+
+    return(
+
+        <>
+            <Navbar>
+            </Navbar>
+        </>
+    );
+  
+}
---
+++ src/components/UI/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+
+export function Navbar(){
+
+    return(
+
+        <>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </>
+
+    );
+}

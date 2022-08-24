9d6e7cf4adae0ea030284ed5a251997aa2c40ef6
galluccinahuel <galluccinahuel@gmail.com>
2022-08-24 16:19:41 -0300

    commit

--- src/components/UI/CartWidget/CartWidget.jsx
+++ src/components/UI/CartWidget/CartWidget.jsx
@@ -1,13 +1,15 @@
 
+import "./CartWidget.css";
+
 import CarWingetIcon from '../CartWidget/CarWingetIcon.png';
 
 export function CartWidget(){
 
     return(
 
-        <>
-            <img src={CarWingetIcon} />   
-        </>
+        <div>
+            <img className='CartWidgetIcon' src={CarWingetIcon} />   
+        </div>
         
     );
 

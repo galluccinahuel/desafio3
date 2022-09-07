7fd38864c13df4dec8388d2ee1a3e1f1b79d8b49
galluccinahuel <galluccinahuel@gmail.com>
2022-09-06 21:21:51 -0300

    apis3

--- src/components/UI/ItemDetail.js/ItemDetail.jsx
+++ src/components/UI/ItemDetail.js/ItemDetail.jsx
@@ -6,6 +6,8 @@ export function ItemDetail({detalleProducto}){
         <>
         <h1>{detalleProducto.title}</h1>
         <h2>{detalleProducto.price}</h2>
+        <h2>{detalleProducto.descripcion}</h2>
+
         </>
     )
 

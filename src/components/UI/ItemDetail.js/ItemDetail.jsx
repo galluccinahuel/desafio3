8bdbf05f4f45c973f64ae45bddb6a17b65ee4478
galluccinahuel <galluccinahuel@gmail.com>
2022-09-11 02:11:21 -0300

    linkcss

--- src/components/UI/ItemDetail.js/ItemDetail.jsx
+++ src/components/UI/ItemDetail.js/ItemDetail.jsx
@@ -1,15 +1,21 @@
 
+
+import "./ItemDetail.css";
+
 export function ItemDetail({detalleProducto}){
 
     const {id, title, price, pictureURL,categoria, descripcion} = detalleProducto;
 
     return(
-        <>
-            <h1>{title}</h1>
-            <h2>{price}</h2>
-            <h2>{descripcion}</h2>
-            <h2>{categoria}</h2>
-            <h2>{pictureURL}</h2>
-        </>
+        <div className="itemDetail">
+            <div className="imgDetail">
+                <img src={pictureURL} alt="" />
+            </div>
+            <div  className="infoDetail">
+                <h2>{title}</h2>
+                <h2>{price}</h2>
+                <h3>{descripcion}</h3>
+            </div>
+        </div>
     )
 }
